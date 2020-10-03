77dd54fe814b773ebe9f262d32e76cb0c64950d9
Louis Grellet <louis.grellet@gmail.com>
2020-10-04 00:50:27 +0200

    clean

--- app.js
+++ app.js
@@ -2,42 +2,40 @@
 // - window.$        [https://code.jquery.com/jquery-1.11.3.min.js]
 // - window.Cookies  [https://github.com/js-cookie/js-cookie]
 
-window.Utils = {
+$.extend($, {
   getUrlParams: function(key, url) {
-    var params = {};
-    url = (url || window.location.search).split('?')[1] || '';
-    url.replace(/([^=&]+)=([^&]*)/g, function(m, key, value) { params[decodeURIComponent(key)] = decodeURIComponent(value); })
-    return key ? params[key] : params;
+    var params = {}
+    url = (url || window.location.search).split('?')[1] || ''
+    url.replace(/([^=&]+)=([^&]*)/g, function(m, key, value) { params[decodeURIComponent(key)] = decodeURIComponent(value) })
+    return key ? params[key] : params
   },
   buildUrl: function(url, params) {
-    url += url.indexOf("?") === -1 ? "?" : "&";
-    return url + $.param(params);
+    url += url.indexOf("?") === -1 ? "?" : "&"
+    return url + $.param(params)
   },
   buildLinks: function(pattern, params) {
     var links = $('a[href*="'+pattern+'"]')
-    links.each(function(i, a) { a.href = Utils.buildUrl(a.href, params); });
-    console.log('buildLinks', links.length)
+    links.each(function(i, a) { a.href = $.buildUrl(a.href, params) })
   }
-};
+})
 
 window.App = {
-  query: Utils.getUrlParams(),
+  query: $.getUrlParams(),
   tracking: {},
   mkts: {}
-};
+}
 
 App.init = function() {
-  App.setTracking(App.query);
-  App.setMkts();
-  Utils.buildLinks('/welcome', $.extend({}, App.tracking, App.mkts));
+  App.setTracking(App.query)
+  App.setMkts()
+  $.buildLinks('/welcome', $.extend({}, App.tracking, App.mkts))
 }
 
 App.setTracking = function(data) {
-  var tracking = $.extend({}, JSON.parse(Cookies.get('im_tracking') || '{}'), data);
-  if (!tracking.subrefferrer && document.referrer) tracking.subrefferrer = document.referrer;
-  Cookies.set('im_tracking', JSON.stringify(tracking), { expires: 1, path: '/' });
+  var tracking = $.extend({}, JSON.parse(Cookies.get('im_tracking') || '{}'), data)
+  if (!tracking.subrefferrer && document.referrer) tracking.subrefferrer = document.referrer
+  Cookies.set('im_tracking', JSON.stringify(tracking), { expires: 1, path: '/' })
   App.tracking = tracking
-  console.log('setTracking', App.tracking)
 }
 
 App.setMkts = function(data) {
@@ -46,21 +44,21 @@ App.setMkts = function(data) {
     data = {}
     var subdir = location.href.split('/')[1]
 
-    if(subdir) data.mkt_source = subdir;
+    if(subdir) data.mkt_source = subdir
 
     // will contain the page title if available (content title / testimonials title)
-    if($('.post-title').text()) data.mkt_content = $('.post-title').text();
+    if($('.post-title').text()) data.mkt_content = $('.post-title').text()
 
     if(subdir == 'content') {
-      var tagHashs = $('body').attr("class").split(/\s+/).filter(function(c) { return c.startsWith('tag-hash-'); });
-      if(tagHashs.length >= 2) data.mkt_campaign = tagHashs[1].replace('tag-hash-', '');
+      var tagHashs = $('body').attr("class").split(/\s+/).filter(function(c) { return c.startsWith('tag-hash-') })
+      if(tagHashs.length >= 2) data.mkt_campaign = tagHashs[1].replace('tag-hash-', '')
     }
 
     App.mkts = data
   }
-  console.log('setMkts', App.mkts)
 }
 
 $(function() {
-  App.init();
-});
+  App.init()
+  console.log('App', App)
+})
